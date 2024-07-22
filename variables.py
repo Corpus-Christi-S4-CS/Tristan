7c2a464ed17ea8238ff37b77ded2bc294117a991
Tristan Hill <tphwoodlands@gmail.com>
2024-07-22 11:23:25 +0100

    exercise

--- variables.py
+++ variables.py
@@ -2,3 +2,12 @@ name = "Tristan"
 age = 18
 weight = 84.2
 
+
+temp = input("Please enter the temperature")
+
+if temp >= 24.0:
+    print("Hot")
+elif (temp > 22.2 and temp < 24.0):
+    print("warm")
+else: 
+    print("Cold")
