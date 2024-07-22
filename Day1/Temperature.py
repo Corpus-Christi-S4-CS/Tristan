dfa79b470ed32d110cd9c773dfa3d579c07c7122
Tristan Hill <tphwoodlands@gmail.com>
2024-07-22 11:32:36 +0100

    working

--- Day1/Temperature.py
+++ Day1/Temperature.py
@@ -1,13 +1,8 @@
-name = "Tristan"
-age = 18
-weight = 84.2
-
-
-temp = input("Please enter the temperature")
+temp = float(input("Please enter the temperature: "))
 
 if temp >= 24.0:
     print("Hot")
-elif (temp > 22.2 and temp < 24.0):
+elif (temp >= 22.2 and temp < 24.0):
     print("warm")
 else: 
     print("Cold")
